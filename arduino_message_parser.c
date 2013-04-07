804267495eb682dc97861d368d8221235c6338f8
Thomas Mayer <thomas@residuum.org>
2013-04-07 23:03:58 +0200

    Added License.

--- arduino_message_parser.c
+++ arduino_message_parser.c
@@ -9,6 +9,26 @@ p = Bits for port
 v = Bits for value
 
 Output: list (a|d) (port as integer) (value as integer)
+
+Copyright (c) 2013 Thomas Mayer
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
 */
 
 #include "m_pd.h"
@@ -26,7 +46,7 @@ void new_byte(t_arduino_message_parser *x, t_floatarg new_value) {
 	int port = 0;
 	char port_type;
 	int value = 0;
-	int new_value_int = (int)new_value;
+	int new_value_int = new_value;
 
 	if (prev_byte > 0) {
 		port_type = 'a';
