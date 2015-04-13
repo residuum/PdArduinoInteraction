d2bede4a1fe4ffa3a5aeaa19606ef313b97f4c79
Thomas Mayer <thomas@residuum.org>
2015-04-13 21:17:40 +0200

    Better data format.

--- arduino_message_parser.c
+++ arduino_message_parser.c
@@ -1,16 +1,16 @@
 /*
-Parses bytes from Arduino in special format:
-Digital message (1 Byte):
-1 0 0 p p p p v
-Analog message (2 Byte):
-0 0 p p p v v v    1 v v v v v v v
+   Parses bytes from Arduino in special format:
+   Digital message (1 Byte):
+   1 0 0 p p p p v
+   Analog message (2 Byte):
+   1 1 p p p v v v    0 v v v v v v v
 
-p = Bits for port
-v = Bits for value
+   p = Bits for port
+   v = Bits for value
 
 Output: list (a|d) (port as integer) (value as integer)
 
-Copyright (c) 2013 Thomas Mayer
+Copyright (c) 2013-2015 Thomas Mayer
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -44,45 +44,46 @@ typedef struct _arduino_message_parser {
 void new_byte(t_arduino_message_parser *x, t_floatarg new_value) {
 	int prev_byte = x->first_byte;
 	int port = 0;
-	char port_type;
+	char port_type = 0;
 	int value = 0;
 	int new_value_int = new_value;
 
-	if (prev_byte > 0) {
+	if (prev_byte > 0 && (new_value_int & 0x80) == 0) { /* analog, 2nd byte */
 		port_type = 'a';
 		port = prev_byte >> 3;
-		value = ((prev_byte & 7) << 7) + (new_value_int & 127);
+		value = ((prev_byte & 7) << 7) + (new_value_int & 0x7f);
 		x->first_byte = 0;
-	} else if ((new_value_int & 224) == 128) {
+	} else if ((new_value_int & 0xc0) == 0x80) { /* digital */
 		port_type = 'd';
-		port = (new_value_int & 127) >> 1;
+		port = (new_value_int & 0x7f) >> 1;
 		value = new_value_int & 1;
-		x->first_byte = 0;
-	} else if ((new_value_int & 128) == 0) {
+	} else if ((new_value_int & 0xc0) == 0xc0) { /*analog, 1st byte */
 		x->first_byte = new_value_int;
 	}
-	if (port_type == 'a' || port_type == 'd') {
-		t_atom out[3];
-		char out_data[2];
-		out_data[2] = 0x00;
-		sprintf(out_data, "%c", port_type);
-		SETSYMBOL(&out[0], gensym(out_data));
-		SETFLOAT(&out[1], port);
-		SETFLOAT(&out[2], value);
-        outlet_list(x->x_obj.ob_outlet, &s_list, 3, &out[0]);
-	}	
+	if (port_type == 0) {
+		return;
+	}
+	t_atom out[3];
+	char out_data[2];
+	out_data[2] = 0x00;
+	sprintf(out_data, "%c", port_type);
+	SETSYMBOL(&out[0], gensym(out_data));
+	SETFLOAT(&out[1], port);
+	SETFLOAT(&out[2], value);
+	outlet_list(x->x_obj.ob_outlet, &s_list, 3, &out[0]);
 }
 
 void *arduino_message_parser_new(void) {
 	t_arduino_message_parser *x = (t_arduino_message_parser *)pd_new(arduino_message_parser_class);
 	outlet_new(&x->x_obj, &s_list);
+	x->first_byte = 0;
 	return (void *)x;
 }
 
 void arduino_message_parser_setup(void) {
 	arduino_message_parser_class = class_new(gensym("arduino_message_parser"),
-		(t_newmethod)arduino_message_parser_new,
-		0, sizeof(t_arduino_message_parser), 0, 0);
+			(t_newmethod)arduino_message_parser_new,
+			0, sizeof(t_arduino_message_parser), 0, 0);
 	class_addfloat(arduino_message_parser_class, new_byte);
 }
 
--- live_performance/live_performance.ino
+++ live_performance/live_performance.ino
@@ -1,33 +1,33 @@
 /*
-Sends data via serial line in special format:
-Digital message (1 Byte):
-1 0 0 p p p p v
-Analog message (2 Bytes):
-0 0 p p p v v v     1 v v v v v v v
+   Sends data via serial line in special format:
+   Digital message (1 Byte):
+   1 0 0 p p p p v
+   Analog message (2 Bytes):
+   1 1 p p p v v v     0 v v v v v v v
 
-p = Bits for port
-v = Bits for value
+   p = Bits for port
+   v = Bits for value
 
-Copyright (c) 2013 Thomas Mayer
+   Copyright (c) 2013-2015 Thomas Mayer
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+ */
 
 #define FOOT_1 2
 #define FOOT_2 3
@@ -40,6 +40,9 @@ THE SOFTWARE.
 
 #define RESISTOR_KEYPAD 1
 #define RESISTOR_R 2
+
+#define DELAY 10
+#define ITERATIONS 10
 
 int footState_1 = LOW;
 int footState_2 = LOW;
@@ -55,90 +58,89 @@ int resistorValue_Keypad = 0;
 int analogChange;
 
 void setup(){
-  Serial.begin(9600);
- 
-  pinMode(FOOT_1, INPUT);
-  pinMode(FOOT_2, INPUT);
-  pinMode(FOOT_3, INPUT);
-  pinMode(HAND_R_1, INPUT);
-  pinMode(HAND_R_2, INPUT);
-  pinMode(HAND_R_3, INPUT);
+	Serial.begin(9600);
 
-  analogChange = 0;
+	pinMode(FOOT_1, INPUT);
+	pinMode(FOOT_2, INPUT);
+	pinMode(FOOT_3, INPUT);
+	pinMode(HAND_R_1, INPUT);
+	pinMode(HAND_R_2, INPUT);
+	pinMode(HAND_R_3, INPUT);
+
+	analogChange = 0;
 }
 
 void loop() {
-  readDigitalValue(FOOT_1, footState_1);
-  readDigitalValue(FOOT_2, footState_2);
-  readDigitalValue(FOOT_3, footState_3);
-  readDigitalValue(HAND_R_1, handRightState_1);
-  readDigitalValue(HAND_R_2, handRightState_2);
-  readDigitalValue(HAND_R_3, handRightState_3);
-  readRoughValue(RESISTOR_KEYPAD, resistorValue_Keypad);
-  if (analogChange == 20) {
-    readAnalogValue(RESISTOR_R, resistorValue_R);
-    analogChange = 0;
-  }
-  delay(10); 
-  analogChange += 1;
+	readDigitalValue(FOOT_1, footState_1);
+	readDigitalValue(FOOT_2, footState_2);
+	readDigitalValue(FOOT_3, footState_3);
+	readDigitalValue(HAND_R_1, handRightState_1);
+	readDigitalValue(HAND_R_2, handRightState_2);
+	readDigitalValue(HAND_R_3, handRightState_3);
+	readRoughValue(RESISTOR_KEYPAD, resistorValue_Keypad);
+	if (analogChange >= ITERATIONS) {
+		readAnalogValue(RESISTOR_R, resistorValue_R);
+		analogChange = 0;
+	}
+	delay(DELAY); 
+	analogChange += 1;
 
 }
-
 
 /**
  * Creates a message for digital value and port number.
  */ 
 void makeDigitalMessage (int port, int value, byte *data) {
-  data[0] = 0x80 | (port << 1) | value;
+	data[0] = 0x80 | (port << 1) | value;
 }
 
 /*
  * Creates a message for analog value and port number.
  */
 void makeAnalogMessage (int port, int value, byte *data) {
-  data[0] = 0 | (port<<3) | ((value >> 7) &0xff);
-  data[1] = 0x80 | (value &0x7F);
+	data[0] = 0xc0 | (port<<3) | ((value >> 7) & 0xff);
+	data[1] = 0x00 | (value & 0x7F);
 }
 
 /**
  * Sends message via Serial port.
  */
 void sendMessage(byte *data, int arraySize) {
-  int i;
-  for (i = 0; i < arraySize; i++) {
-    /* for Arduino < 1.0 */
-    /*Serial.print(data[i], BYTE);*/
-    /* for Arduino > 1.0 */
-    Serial.write(data[i]);
-  } 
+	int i;
+	for (i = 0; i < arraySize; i++) {
+		/* for Arduino < 1.0 */
+		/*Serial.print(data[i], BYTE);*/
+		/* for Arduino > 1.0 */
+		Serial.write(data[i]);
+	} 
 }
 
 /**
  * Reads a digital value and outputs the value, when it has changed.
  */
 void readDigitalValue(int trigger, int &value) {
-  int arraySize = 1;
-  int tempValue = digitalRead(trigger);
-  if (value != tempValue) {
-    value = tempValue;
-    byte data[arraySize];
-    makeDigitalMessage(trigger, value, data);
-    sendMessage(data, arraySize);
-  }
+	int arraySize = 1;
+	int tempValue = digitalRead(trigger);
+	if (value != tempValue) {
+		value = tempValue;
+		byte data[arraySize];
+		makeDigitalMessage(trigger, value, data);
+		sendMessage(data, arraySize);
+	}
 }
 
 /**
  * Reads a analog value and outputs the value, when it has changed.
  */
 void readAnalogValue(int trigger, int &value) {
-  int arraySize = 2;
-  int tempValue = analogRead(trigger);
-  if (value != tempValue) {
-    value = tempValue;
-    byte data[arraySize];
-    makeAnalogMessage(trigger, value, data);
-    sendMessage(data, arraySize);
-  }
+	int arraySize = 2;
+	int tempValue = analogRead(trigger);
+	if (value != tempValue) {
+		value = tempValue;
+		byte data[arraySize];
+		makeAnalogMessage(trigger, value, data);
+		sendMessage(data, arraySize);
+	}
 }
 
 /**
@@ -148,12 +150,12 @@ void readAnalogValue(int trigger, int &value) {
  * Useful for Keypads and resistor array.
  */
 void readRoughValue(int trigger, int &value) {
-  int arraySize = 2;
-  int tempValue = analogRead(trigger);
-  if (abs(value - tempValue) > ANALOG_ROUGHNESS) {
-    value = tempValue;
-    byte data[arraySize];
-    makeAnalogMessage(trigger, value, data);
-    sendMessage(data, arraySize);
-  }
+	int arraySize = 2;
+	int tempValue = analogRead(trigger);
+	if (abs(value - tempValue) > ANALOG_ROUGHNESS) {
+		value = tempValue;
+		byte data[arraySize];
+		makeAnalogMessage(trigger, value, data);
+		sendMessage(data, arraySize);
+	}
 }
